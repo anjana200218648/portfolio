2e14db8ae41ad88a248d96133808e5dd9290ae75
H.H.H.Anjana <146448020+anjana200218648@users.noreply.github.com>
2024-06-18 22:35:29 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,243 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "certifications", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Creative web developer transforming ideas into dynamic online experiences. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/n1.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm a web developer who's highly adaptable with time zone communications.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "I'm a tech enthusiast with a passion for web development..",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Fultter Maths App",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/n2.jpeg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to connact me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Electrical shop Management System",
+    des: "A web based application that was developed for undergraduate group project with a real world problem and a solution.",
+    img: "/n3.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/mongodb.svg", "/js.svg"],
+    link: "/https://github.com/anjana200218648/Payment_React.git",
+  },
+  {
+    id: 2,
+    title: "RANK Vehicle Service",
+    des: "A web-based system was developed by me with a real-world problem and a solution.",
+    img: "/n4.png",
+    iconLists: ["/java.svg", "/mysql.svg", "/bootstrap.svg","/css.svg"],
+    link: "/https://github.com/anjana200218648/rental_system_full-.git",
+  },
+  {
+    id: 3,
+    title: "Number Calculation Game",
+    des: "A mobile Application that I made to enhance the mathematical knowledge of children.",
+    img: "/n5.png",
+    iconLists: ["/kotlin.svg", "/sqlite.svg", "/and.svg", "/java.svg", ],
+    link: "/https://github.com/anjana200218648/Quiz_game.git",
+  },
+  {
+    id: 4,
+    title: "NOTE+ App",
+    des: "The next generation mobile note app with new features more than to normal note app..",
+    img: "/n6.jpg",
+    iconLists: ["/kotlin.svg", "/sqlite.svg", "/and.svg", "/java.svg"],
+    link: "/https://github.com/anjana200218648/Kotlin_Full_Note_App-CRUD-.git",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "certificate of completion for an online non-credit course titled Programming Fundamentals in Kotlin, authorized by Meta and offered through Coursera. It confirms that HEWA HAPLAGE HASHAN ANJANA has successfully completed the course. The certificate can be verified online at the provided URL.",
+    name: "Programming Fundamentals in Kotlin",
+    title: "Meta",
+  },
+  {
+    quote:
+      "certificate of completion for an online non-credit course titled Start the UX Design Process: Empathize, Define, and Ideate,authorized by Google and offered through Coursera. It confirms that HEWA HAPLAGE HASHAN ANJANA has successfully completed the course. The certificate is signed by Amanda Brophy, Global Director of Google Career Certificates, and can be verified online at the provided URL.",
+    name: "UI/UX Design (level 3)",
+    title: "Google",
+  },
+  {
+    quote:
+      "This competition focused on biomimicry and was conducted in Sinhala/Tamil medium. It was organized by the Science Education and Dissemination Unit of the National Institute of Fundamental Studies (NIFS) in Kandy, Sri Lanka. The certificate is signed by Dr. Kumari Tilakaratne, representing the Science Education and Dissemination Unit, and Prof. S.H.P.P. Karunaratne, the Director of NIFS.",
+    name: "All island Competition on Problem Sloving",
+    title: "NIFSC",
+  },
+  {
+    quote:
+      " Web Design for Beginners and Python for Beginners. Both programs were conducted by the University of Moratuwa, Sri Lanka, through the Centre for Open & Distance Learning (CODL). The Web Design for Beginners course was organized by the Department of Information Technology, Faculty of Information Technology, while the Python for Beginners course was managed by the Department of Computer Science & Engineering, Faculty of Engineering. Both certificates are endorsed by the respective department heads and the director of CODL.",
+    name: "Python,Web Design",
+    title: "UOM",
+  },
+  
+];
+
+export const companies = [
+  {
+    id: 1,
+    nameImg: "/java.svg",
+  },
+  {
+    id: 2,
+    nameImg: "/kotlin.svg",
+  },
+  {
+    id: 3,
+   
+    nameImg: "/php.svg",
+  },
+  {
+    id: 8,
+   
+    nameImg: "/c++.svg",
+  },
+  {
+    id: 4,
+    nameImg: "/re.svg",
+  },
+   {
+    id: 9,
+    nameImg: "/js.svg",
+  },
+  
+  {
+    id: 5,
+    
+    nameImg: "/python.svg",
+  },
+   {
+    id: 6,
+    
+    nameImg: "/bootstrap.svg",
+  },
+  {
+    id: 7,
+    
+    nameImg: "/tail.svg",
+  },
+  {
+    id: 10,
+    
+    nameImg: "/mysql.svg",
+  },
+  {
+    id: 11,
+    
+    nameImg: "/mongodb.svg",
+  },
+  {
+    id: 12,
+    
+    nameImg: "/flutter.svg",
+  },
+
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Using react,tailwind,next.js,bootstrap ect...",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile Application development Intern ",
+    desc: "Using Android studio(kotlin or java) or Flutter ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Devops Engineer Intern",
+    desc: "At present , the necessary knowledge is being collected",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Backend Developer Intern",
+    desc: "Using php,mysql,mongodb ect...",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    link:"/https://github.com/anjana200218648",
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    link:"/https://anjanatecblog.wordpress.com/",
+    img: "/w2.svg",
+  },
+  {
+    id: 3,
+    link:"/https://www.linkedin.com/in/h-h-h-anjana-62bb18254/",
+    img: "/link.svg",
+  },
+];
